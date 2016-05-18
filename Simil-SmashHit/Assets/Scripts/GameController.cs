c50bb38e185747ca7e64336628abebce440ca676
mattia ristori <mattia ristori>
2016-05-18 09:49:44 +0200

    PROGETTO INIZIALE

---
+++ Simil-SmashHit/Assets/Scripts/GameController.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour 
+{
+	public GameObject _sphere;
+	public int _numberOfSphere;
+	public Text _sphereText;
+	public Text _gameOverText;
+	public Text _restartText;
+
+
+	// Use this for initialization
+	private bool _gameover;
+
+	void Start () 
+	{
+		_gameOverText.text = "";
+		_restartText.text = "";
+		_gameover = false;
+		_numberOfSphere = 10;
+	}
+
+	void Update()
+	{
+		_sphereText.text = "SPHERE: " + _numberOfSphere;
+
+		if (Input.GetKeyDown (KeyCode.R) && _gameover) 
+		{
+			SceneManager.LoadScene ("Main");
+		}
+
+
+		if (_numberOfSphere == 0) 
+		{
+			_gameover = true;
+			if (_gameover) 
+			{
+				GameOver ();
+			}
+		}
+
+		if (Input.GetMouseButtonDown(0) && !_gameover)
+		{
+			_numberOfSphere--;
+			Vector3 mouseWorldPoint = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,Input.mousePosition.y,5));
+			Instantiate (_sphere, mouseWorldPoint, transform.rotation);
+		}
+	}
+
+	public void GameOver()
+	{
+		_gameOverText.text = "GAME OVER";
+		_restartText.text = "Press R to restart";
+	}
+		
+}
---
+++ Simil-SmashHit/Assets/Scripts/MoveSphere.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveSphere : MonoBehaviour {
+
+	public float _speed;
+
+	private GameController _gameController;
+
+	void Start () 
+	{
+		GetComponent<Rigidbody> ().velocity = transform.forward * _speed;
+
+		GameObject gameControllerObject = GameObject.FindGameObjectWithTag ("gameController");
+		if (gameControllerObject != null)
+		{
+			_gameController = gameControllerObject.GetComponent <GameController>();
+		}
+		if (_gameController == null)
+		{
+			Debug.Log ("Cannot find 'GameController' script");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Obj") 
+		{
+			_gameController._numberOfSphere += 3;
+			Destroy (other.gameObject);
+		}
+	}
+}
