cf3b6b486af384d218fc24fa9e8cf2c478dba72e
Alessandro <sandro94@live.it>
2016-05-19 11:29:36 +0200

    Boundary

---
+++ Simil-SmashHit/Assets/Scripts/DestroyByBoundary.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByBoundary : MonoBehaviour {
+	public float _speedBoundary;
+	private Rigidbody _rb;
+	void Start()
+	{
+		_rb = GetComponent<Rigidbody> ();
+		_rb.velocity = transform.forward * _speedBoundary;
+
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		Destroy (other.gameObject);
+	}
+}
